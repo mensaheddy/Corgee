b898edeab290041590d21690e565959c93cd58c1
Edwin Mensah <edwinmensah82@gmail.com>
2020-12-11 17:55:22 +0100

    Intergrate exception handlers to application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
+  include ExceptionHandlers
 
   protect_from_forgery with: :null_session
+  before_action :authenticate_user!
 end
